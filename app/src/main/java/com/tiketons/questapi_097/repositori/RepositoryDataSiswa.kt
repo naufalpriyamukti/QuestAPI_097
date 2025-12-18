27d134542914ea8e3307705c84380265337a162a
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-18 17:31:53 +0700

    Tambah fungsi suspend postDataSiswa(dataSiswa: DataSiswa) untuk mengirim data siswa

--- app/src/main/java/com/tiketons/questapi_097/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/tiketons/questapi_097/repositori/RepositoryDataSiswa.kt
@@ -2,4 +2,5 @@ package com.example.questapi_097.repositori
 
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
