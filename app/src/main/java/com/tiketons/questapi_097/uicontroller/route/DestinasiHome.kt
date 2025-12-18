344c2848299bdab45b7e7ac0de4bc2a2b4ddafe7
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-18 19:04:33 +0700

    Tambah objek rute untuk home screen

--- app/src/main/java/com/tiketons/questapi_097/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/tiketons/questapi_097/uicontroller/route/DestinasiHome.kt
@@ -1,5 +1,7 @@
 package com.tiketons.questapi_097.uicontroller.route
 
+import com.tiketons.questapi_097.R
+
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
