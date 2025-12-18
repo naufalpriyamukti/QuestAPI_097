96e284e9fd38e301919be63de98d0e616a152b04
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-18 18:24:48 +0700

    Menambahkan navigasi composable untuk alur data siswa

---
+++ app/src/main/java/com/tiketons/questapi_097/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,48 @@
+package com.tiketons.questapi_097.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tiketons.questapi_097.uicontroller.route.DestinasiHome
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                navigateToItemUpdate = {
+                    // navController.navigate("${DestinasiDetail.route}/$it")
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
