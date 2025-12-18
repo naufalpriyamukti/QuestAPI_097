90148ff77efd3a54c47cabc0a9f2ddd36c1ce1aa
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-18 17:33:39 +0700

    Implementasi JaringanRepositoryDataSiswa untuk interaksi dengan ServiceApiSiswa

--- app/src/main/java/com/tiketons/questapi_097/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/tiketons/questapi_097/repositori/RepositoryDataSiswa.kt
@@ -4,3 +4,10 @@ interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
