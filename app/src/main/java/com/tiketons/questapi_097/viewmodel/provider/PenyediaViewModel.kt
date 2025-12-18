170dc9c4e1c13de7f5fcc463301713c5251e640a
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-18 19:07:20 +0700

    Tambah PenyediaViewModel untuk pembuatan ViewModel dengan dependency injection

---
+++ app/src/main/java/com/tiketons/questapi_097/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.tiketons.questapi_097.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.tiketons.questapi_097.viewmodel.EntryViewModel
+import com.tiketons.questapi_097.viewmodel.HomeViewModel
+import com.tiketons.questapi_97.repositori.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa)
+        }
+    }
+}
