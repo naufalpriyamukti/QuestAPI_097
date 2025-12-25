f0a71dcc650ab72d9726dce089311d1e93de8dba
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:39:03 +0700

    Menentukan idSiswa dari savedStateHandle dan menginisialisasi uiStateSiswa dengan data siswa

--- app/src/main/java/com/tiketons/questapi_097/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/tiketons/questapi_097/viewmodel/EditViewModel.kt
@@ -4,4 +4,11 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDa
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 }
