918f97795f664f21ad8c168d48a2af2d287908e8
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:57:40 +0700

    Menambahkan komponen DetailSiswaScreen dengan parameter navigasi dan ViewModel

---
+++ app/src/main/java/com/tiketons/questapi_097/view/HalamanDetail.kt
@@ -0,0 +1,19 @@
+package com.tiketons.questapi_097.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tiketons.questapi_097.viewmodel.DetailViewModel
+import com.tiketons.questapi_097.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
