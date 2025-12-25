335be11d9a32101a86dc2d6424b666e878df1acd
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:06:43 +0700

    Menambahkan import retrofit untuk PUT, DELETE, Query

--- app/src/main/java/com/tiketons/questapi_097/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/tiketons/questapi_097/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,13 @@ package com.tiketons.questapi_097.apiservice
 
 import com.tiketons.questapi_097.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+
 interface ServiceApiSiswa{
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
