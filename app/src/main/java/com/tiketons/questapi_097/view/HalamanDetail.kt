ef995a8c23fb7d74a75228d9faf7d70dd67ffb31
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:58:40 +0700

    Menambahkan komponen Scaffold di DetailSiswaScreen dengan AppBar, FloatingActionButton, dan BodyDetailDataSiswa

--- app/src/main/java/com/tiketons/questapi_097/view/HalamanDetail.kt
+++ app/src/main/java/com/tiketons/questapi_097/view/HalamanDetail.kt
@@ -16,4 +16,58 @@ fun DetailSiswaScreen(
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
 
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.statusUIDetail
+            FloatingActionButton(
+                onClick = {
+                    when(uiState){
+                        is StatusUIDetail.Success->
+                            navigateToEditItem(uiState.satusiswa.id)
+                        else->{}
+                    }
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        },
+        modifier = modifier
+    ) { innerPadding ->
+
+        val coroutineScope = rememberCoroutineScope()
+
+        BodyDetailDataSiswa(
+            statusUIDetail = viewModel.statusUIDetail,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.hapusSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
+@Composable
+private fun BodyDetailDataSiswa(
+    statusUIDetail: StatusUIDetail,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
 }
