e639cde1552494bc47c08d5a5eddbfd540a989c9
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:40:41 +0700

    Menambahkan validasiInput untuk memeriksa kelayakan data siswa dan fungsi editSatuSiswa untuk memperbarui data siswa

--- app/src/main/java/com/tiketons/questapi_097/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/tiketons/questapi_097/viewmodel/EditViewModel.kt
@@ -15,4 +15,22 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDa
     fun updateUiState(detailSiswa: DetailSiswa){
         uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa.detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful){
+                println("Update Sukses : ${call.message()}")
+            }else{
+                println("Update Error : ${call.errorBody()}")
+            }
+        }
+    }
 }
