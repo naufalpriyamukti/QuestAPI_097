48db334dab2bfcf7af55e771e4d20c7da4544a03
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:38:10 +0700

    Menambahkan variabel uiStateSiswa dengan tipe UIStateSiswa menggunakan mutableStateOf

--- app/src/main/java/com/tiketons/questapi_097/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/tiketons/questapi_097/viewmodel/EditViewModel.kt
@@ -1,5 +1,7 @@
 package com.tiketons.questapi_097.viewmodel
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
 }
