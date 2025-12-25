dc182586724081a507ed91d7a0debdabd89b61e0
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:45:35 +0700

    Mendefinisikan idSiswa dari savedStateHandle dan menambahkan statusUIDetail dengan nilai awal Loading

--- app/src/main/java/com/tiketons/questapi_097/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/tiketons/questapi_097/viewmodel/DetailViewModel.kt
@@ -7,5 +7,11 @@ sealed interface StatusUIDetail {
     object Loading : StatusUIDetail
 }
 class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
 
+    init {
+        getSatuSiswa()
+    }
 }
