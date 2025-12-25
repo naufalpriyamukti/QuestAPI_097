c42b2cf2ff4008523843c7fcf34c2d55aced5e81
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:33:02 +0700

    Menambahkan navigasi ke DetailSiswaScreen dengan argumen ID dan fungsi navigasi kembali

--- app/src/main/java/com/tiketons/questapi_097/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/tiketons/questapi_097/uicontroller/PetaNavigasi.kt
@@ -47,5 +47,19 @@ fun HostNavigasi(
                 }
             )
         }
+
+
+        composable(
+            DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            DetailSiswaScreen(
+                navigateToEditItem = { navController.navigate("${DestinasiEdit.route}/$it") },
+                navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+
+
+
     }
 }
