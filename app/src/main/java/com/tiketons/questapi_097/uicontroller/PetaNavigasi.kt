df8e7e68b25dc70eee751c7e44910521d4def26f
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:33:43 +0700

    Menambahkan navigasi ke EditSiswaScreen dengan argumen ID dan fungsi navigasi kembali

--- app/src/main/java/com/tiketons/questapi_097/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/tiketons/questapi_097/uicontroller/PetaNavigasi.kt
@@ -59,6 +59,13 @@ fun HostNavigasi(
                 navigateBack = { navController.navigate(DestinasiHome.route) })
         }
 
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg){
+            type= NavType.IntType
+        })){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)},
+                onNavigateUp = { navController.navigateUp()})
+        }
+
 
 
     }
