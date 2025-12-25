3a39532d68ce59c52a55a0cc903db174ce885562
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 19:09:36 +0700

    Menambahkan komponen EditSiswaScreen dengan AppBar, EntrySiswaBody, dan pengelolaan aksi simpan

---
+++ app/src/main/java/com/tiketons/questapi_097/view/HalamanEdit.kt
@@ -0,0 +1,38 @@
+package com.tiketons.questapi_097.view
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+
+        val coroutineScope = rememberCoroutineScope()
+
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
