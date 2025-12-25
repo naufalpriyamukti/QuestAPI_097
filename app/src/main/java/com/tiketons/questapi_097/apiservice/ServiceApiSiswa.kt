025f6b174da8f96759c801257c5dd6d1b3cf9ed9
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:04:40 +0700

    Menambahkan fungsi untuk mengedit data data siswa berdasarkan ID

--- app/src/main/java/com/tiketons/questapi_097/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/tiketons/questapi_097/apiservice/ServiceApiSiswa.kt
@@ -14,6 +14,9 @@ interface ServiceApiSiswa{
     @GET("baca1Teman.php/{id}")
     suspend fun getSatuSiswa (@Query("id") id: Int): DataSiswa
 
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa (@Query("id") id: Int, @Body dataSiswa: DataSiswa) : retrofit2. Response<Void>
+
 
 }
 
