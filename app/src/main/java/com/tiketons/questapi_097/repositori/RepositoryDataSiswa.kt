44210106b43c44034ef0841c925607f08e1f8e5e
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 18:19:39 +0700

    Mengoverride fungsi editSatuSiswa untuk menggunakan serviceApiSiswa

--- app/src/main/java/com/tiketons/questapi_097/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/tiketons/questapi_097/repositori/RepositoryDataSiswa.kt
@@ -24,4 +24,7 @@ class JaringanRepositoryDataSiswa(
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 
     override suspend fun getSatuSiswa (id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa (id)
+
+    override suspend fun editSatuSiswa (id: Int, dataSiswa: DataSiswa): retrofit2. Response<Void> = serviceApiSiswa.editSatuSiswa (id, dataSiswa)
+
 }
