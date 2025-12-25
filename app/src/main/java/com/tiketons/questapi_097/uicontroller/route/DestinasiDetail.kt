7501d7bbf01aa91632ca1ad8f1266ddbc86cf5c5
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-12-25 19:04:17 +0700

    Menambahkan objek DestinasiDetail dengan rute, judul, dan argumen ID siswa

--- app/src/main/java/com/tiketons/questapi_097/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/tiketons/questapi_097/uicontroller/route/DestinasiDetail.kt
@@ -1,1 +1,10 @@
 package com.tiketons.questapi_097.uicontroller.route
+
+import com.tiketons.questapi_097.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
